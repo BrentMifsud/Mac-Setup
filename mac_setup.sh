2077fef7404aa1042d47335d9a3adc807f93117a
Brent Mifsud <brentmifsud@icloud.com>
2020-01-06 02:32:16 -0500

    add powerline fonts

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-echo "Begin Setup...";
+echo "Begin Setup..."
 
 ##Check if github is available
 echo "Checking if github is available..."
@@ -8,89 +8,99 @@ SUCCESS=$?
 
 if [ $SUCCESS -eq 0 ]
 then
-  echo "Github is available. Continuing...";
+  echo "Github is available. Continuing..."
 else
-  echo "Setup requires github to complete. Please try on home wifi or get access from network security.";
-  echo "Exiting...";
-  exit 0;
+  echo "Setup requires github to complete. Please try on home wifi or get access from network security."
+  echo "Exiting..."
+  exit 0
 fi
 
 ## Install Homebrew
-echo;
-echo "Installing Homebrew...";
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
-source ~/.zshrc;
+echo
+echo "Installing Homebrew..."
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+source ~/.zshrc
 
-echo;
-echo "Installing applications...";
+echo
+echo "Installing applications from homebrew..."
+
+## Get latest version of git
+brew install git
+
+## get latest version of zsh
+brew install zsh
 
 ## Open JDK
 brew cask install adoptopenjdk
  
 ## Appcleaner
-brew cask install appcleaner;
+brew cask install appcleaner
  
 ## Dbeaver database viewer
-brew cask install dbeaver-community;
+brew cask install dbeaver-community
  
 ## Iterm2
-brew cask install iterm2;
+brew cask install iterm2
  
 ## Jetbrains toolbox
-brew cask install jetbrains-toolbox;
+brew cask install jetbrains-toolbox
  
 ## Postman
-brew cask install postman;
+brew cask install postman
  
 ## Slack
-brew cask install slack;
+brew cask install slack
  
 ## Sourcetree
-brew cask install sourcetree;
+brew cask install sourcetree
  
 ## visual studio code
-brew cask install visual-studio-code;
+brew cask install visual-studio-code
 
-echo;
-echo "Done installing applications."
- 
+echo
+echo "Done."
+echo
+
 ## Install oh-my-zsh
-echo;
-echo "Installing oh-my-zsh...";
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
-source ~/.zshrc;
-
-## zsh autosuggestions
-echo "\n" >> ~/.zshrc;
-brew install zsh-autosuggestions;
-echo "source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc;
-source ~/.zshrc;
- 
-## zsh syntax highlighting
-brew install zsh-syntax-highlighting;
-echo "export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/usr/local/share/zsh-syntax-highlighting/highlighters" >> ~/.zshrc; 
-echo "source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc;
+echo "Installing oh-my-zsh..."
+echo
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 source ~/.zshrc
 
-## Add Aliases
+## zsh autosuggestions
+echo "\n" >> ~/.zshrc
+brew install zsh-autosuggestions
+echo "source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+source ~/.zshrc
+ 
+## zsh syntax highlighting
+brew install zsh-syntax-highlighting
+echo "export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/usr/local/share/zsh-syntax-highlighting/highlighters" >> ~/.zshrc 
+echo "source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+source ~/.zshrc
 
+echo
+echo "done."
+echo
+
+## Add Aliases
 echo "
 ## git aliases
-function gc { git commit -m \"\$@\"; }
-alias gcm=\"git checkout master\";
-alias gs=\"git status\";
-alias gpull=\"git pull\";
-alias gf=\"git fetch\";
-alias gfa=\"git fetch --all\";
-alias gf=\"git fetch origin\";
-alias gpush=\"git push\";
-alias gd=\"git diff\";
-alias ga=\"git add .\";
-alias gb=\"git branch\";
+function gc { git commit -m \"\$@\" }
+alias gcm=\"git checkout master\"
+alias gs=\"git status\"
+alias gpull=\"git pull\"
+alias gf=\"git fetch\"
+alias gfa=\"git fetch --all\"
+alias gf=\"git fetch origin\"
+alias gpush=\"git push\"
+alias gd=\"git diff\"
+alias ga=\"git add .\"
+alias gb=\"git branch\"
 alias gbr=\"git branch remote\"
 alias gfr=\"git remote update\"
 alias gbn=\"git checkout -B \"
-alias grf=\"git reflog\";
+alias grf=\"git reflog\"
 alias grh=\"git reset HEAD~\" # last commit
 alias gac=\"git add . && git commit -a -m \"
 alias gsu=\"git gpush --set-upstream origin \"
@@ -102,14 +112,30 @@ alias refresh=\"source ~/.zshrc\"
 alias update=\"brew update; brew upgrade;\"
 " >> ~/.zshrc
 
-## Setup ssh (comment out if re-running the script)
-echo;
-echo "Setting up ssh key...";
-echo "Press enter when prompted to set ssh password file location if you want to use default settings.";
-echo;
-ssh-keygen;
-echo "SSH keygen complete.";
+## Add repos folders
+mkdir ~/repos
+mkdir ~/repos/iOS
 
-echo;
+## Get all Powerline Fonts
+echo
+echo "Getting Powerline fonts for use with iterm and oh-my-zsh..."
+echo
+
+git clone https://github.com/powerline/fonts.git ~/repos/powerlineFonts
+
+echo
+echo "done."
+echo
+
+## Setup ssh (comment out if re-running the script)
+echo
+echo "Setting up ssh key. Please follow the prompts..."
+echo
+
+ssh-keygen
+
+echo
+echo "done."
+echo
 
 echo "Setup Complete."
